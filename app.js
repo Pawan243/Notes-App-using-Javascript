b51170ca682d7e38319b8f2ee809bea8bcd18932
Pawan Kumar <54316119+Pawan243@users.noreply.github.com>
2021-03-11 16:54:30 +0530

    update Notes App with Title

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ showNotes();
 let addbtn = document.getElementById('addBtn');
 addbtn.addEventListener("click", function (e) {
     let addTxt = document.getElementById("addTxt");
+    let addTitle = document.getElementById("addTitle");
     let notes = localStorage.getItem("notes")
 
     if (notes == null) {
@@ -13,9 +14,14 @@ addbtn.addEventListener("click", function (e) {
     else {
         notesObj = JSON.parse(notes);
     }
-    notesObj.push(addTxt.value);
+    let myObj = {
+        title: addTitle.value,
+        text: addTxt.value
+    }
+    notesObj.push(myObj);
     localStorage.setItem("notes", JSON.stringify(notesObj));
     addTxt.value = "";
+    addTitle.value = "";
     console.log(notesObj);
 
     showNotes();
@@ -33,8 +39,8 @@ function showNotes() {
     notesObj.forEach(function (element, index) {
         html += `<div class="card my-2 mx-2" style="width: 18rem;">
         <div class="card-body">
-            <h5 class="card-title">Note ${index + 1} </h5>
-            <p class="card-text">${element} </p>
+            <h5 class="card-title">${element.title} </h5>
+            <p class="card-text">${element.text} </p>
             <button id = "${index}" onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
         </div>
     </div>`;
